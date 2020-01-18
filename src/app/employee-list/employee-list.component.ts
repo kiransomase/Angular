1378987d36438dd4e3d12ad1d50c177454ba4b0b
unknown <kiran_somase@yahoo.co.in>
2020-01-18 18:48:29 +0530

    This code is uploaded by Kiran on 18/01/2020 at 6:48

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,62 @@
+import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
+import { Observable } from "rxjs";
+import { EmployeeService } from "../employee.service";
+import { Employee } from "../employee";
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: "app-employee-list",
+  templateUrl: "./employee-list.component.html",
+  styleUrls: ["./employee-list.component.css"]
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Observable<Employee[]>;
+
+  constructor(private route: ActivatedRoute,private router: Router,
+    private employeeService: EmployeeService) {}
+    id: number;
+    employee: Employee;
+  ngOnInit() {
+    this.employee = new Employee();
+
+    this.id = this.route.snapshot.params['id'];
+    
+    this.employeeService.getEmployee(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.employee = data;
+      }, error => console.log(error));
+    
+    this.reloadData();
+  }
+    updateEmployee() {
+      this.employeeService.updateEmployee(this.id, this.employee)
+        .subscribe(data => console.log(data), error => console.log(error));
+      this.employee = new Employee();
+      this.gotoList();
+    }
+
+    gotoList() {
+      this.router.navigate(['/employees/id']);
+    }
+
+
+  reloadData() {
+    this.employees = this.employeeService.getEmployeesList();
+  }
+
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  employeeDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+}
